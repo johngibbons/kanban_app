1b72db3a44d52e7493a01443128fc840e940c958
John Gibbons <johngibbons10@gmail.com>
2015-12-24 22:35:00 -0700

    set up webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -12,7 +13,19 @@ module.exports = {
     path: PATHS.build,
     filename: 'bundle.js'
   },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+
+    stats: 'errors-only',
+
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: 'Kanban app'
     })
